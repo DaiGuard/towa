376ebfce826bc97aa0f898d9b82286ed20e9ca78
DaiGuard <zenith.or.w@gmail.com>
2021-11-24 00:09:55 +0900

    道を上書きするテスト中

--- exp/react-google-map/src/App.js
+++ exp/react-google-map/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { URL } from 'url';
+
+const request = require('request');
+const querystring = require('querystring');
+
 
 function App() {
   return (
@@ -29,14 +34,40 @@ const center = {
 const MyComponent = () => {
 
   const API_KEY = process.env.React_APP_GOOGLE_MAP_API_KEY;
+  const path = '35.69575,139.77521|35.79575,139.87521';
+
+  let params = new URLSearchParams();
+  params.append("path", path);
+  params.append("interpolate", true);
+  params.append("key", API_KEY);
+
+  console.log(`API KEY=${API_KEY}`);
+
+  let options = {
+    url: 'https://roads.googleapis.com/v1/snapToRoads' + '?' + params.toString(),            
+    // url: `https://roads.googleapis.com/v1/snapToRoads?path=${path}&interpolate=true&key=${API_KEY}`,
+    method: 'GET',
+    // data: {
+    //   path: '35.69575,139.77521|35.79575,139.87521',
+    //   interpolate: true,
+    //   key: API_KEY
+    // }
+  }
+
+  request(options, function(error, responce, body) {
+    console.log(body);
+  })
 
   return(
-    <LoadScript googleMapsApiKey={API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={17}></GoogleMap>      
-    </LoadScript>
+    <div>
+      <p>TEST</p>
+    </div>
+    // <LoadScript googleMapsApiKey={API_KEY}>
+    //   <GoogleMap
+    //     mapContainerStyle={containerStyle}
+    //     center={center}
+    //     zoom={17}></GoogleMap>      
+    // </LoadScript>
   );
 }
 
