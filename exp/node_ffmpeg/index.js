4996e381e644bbbc372760a66744d75c095621b3
DaiGuard <zenith.or.w@gmail.com>
2021-12-01 00:40:12 +0900

    動画圧縮のサンプルを追加

---
+++ exp/node_ffmpeg/index.js
@@ -0,0 +1,16 @@
+var path = require('path')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+var ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+ffmpeg(path.join(__dirname, 'data', 'test.MP4'))
+  .size('50%')
+  .fps('24')
+  .on('end', () => {
+    console.log('file load succesfully')
+  })
+  .on('error', (err) => {
+    console.log(`error: ${err.message}`);
+  })
+  .save(path.join(__dirname, 'data', 'output.MP4'));
